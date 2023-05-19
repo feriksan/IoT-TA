0e9db2b1e12200eb67efe66e605379c9c777eb49
Feriksan <ferdicool95@gmail.com>
2023-05-19 22:24:04 +0800

    update remove try

--- websocket.py
+++ websocket.py
@@ -6,53 +6,49 @@ import os
 
 async def transmit(websocket):
     print("Client Connected !")
-    try :
-        cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
 
-        while cap.isOpened():
-            _, frame = cap.read()
+    while cap.isOpened():
+        _, frame = cap.read()
 
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            UPPER_HUE = os.getenv('UPPER_HUE')
-            UPPER_SATURATION = os.getenv('UPPER_SATURATION')
-            UPPER_VALUE = os.getenv('UPPER_VALUE')
-            LOWER_HUE = os.getenv('LOWER_HUE')
-            LOWER_SATURATION = os.getenv('LOWER_SATURATION')
-            LOWER_VALUE = os.getenv('LOWER_VALUE')
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        UPPER_HUE = os.getenv('UPPER_HUE')
+        UPPER_SATURATION = os.getenv('UPPER_SATURATION')
+        UPPER_VALUE = os.getenv('UPPER_VALUE')
+        LOWER_HUE = os.getenv('LOWER_HUE')
+        LOWER_SATURATION = os.getenv('LOWER_SATURATION')
+        LOWER_VALUE = os.getenv('LOWER_VALUE')
 
-            lower_hsv = np.array([int(LOWER_HUE), int(LOWER_SATURATION), int(LOWER_VALUE)])
-            upper_hsv = np.array([int(UPPER_HUE), int(UPPER_SATURATION), int(UPPER_VALUE)])
-            
-            mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
-            contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        lower_hsv = np.array([int(LOWER_HUE), int(LOWER_SATURATION), int(LOWER_VALUE)])
+        upper_hsv = np.array([int(UPPER_HUE), int(UPPER_SATURATION), int(UPPER_VALUE)])
+        
+        mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            #looping for contours
-            for c in contours:
-                if cv2.contourArea(c) < 500:
-                    continue
-                    
-                #get bounding box from countour
-                (x, y, w, h) = cv2.boundingRect(c)
+        #looping for contours
+        for c in contours:
+            if cv2.contourArea(c) < 500:
+                continue
                 
-                #draw bounding box
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            #get bounding box from countour
+            (x, y, w, h) = cv2.boundingRect(c)
+            
+            #draw bounding box
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            result = cv2.bitwise_and(frame, frame, mask=mask)
-            encoded = cv2.imencode('.jpg', result)[1]
+        result = cv2.bitwise_and(frame, frame, mask=mask)
+        encoded = cv2.imencode('.jpg', result)[1]
 
-            data = str(base64.b64encode(encoded))
-            data = data[2:len(data)-1]
-            
-            await websocket.send(data)
-            
-            # cv2.imshow("Transimission", frame)
-            
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
-        cap.release()
-    except:
-        print("Someting went Wrong !")
-        cap.release()
+        data = str(base64.b64encode(encoded))
+        data = data[2:len(data)-1]
+        
+        await websocket.send(data)
+        
+        # cv2.imshow("Transimission", frame)
+        
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+    cap.release()
 
 async def sendData(websocket, data):
     await websocket.send(data)
