fd6af35678764356a20ca5a62654c5ac14cc453f
Feriksan <ferdicool95@gmail.com>
2023-05-23 16:08:12 +0800

    aaa

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.ObjectTracking.getMasking import getMasking
 import asyncio
-from websocket import Websocket
-from Firebase.firebaseConnect import FirebaseConnect
+# from websocket import Websocket
+# from Firebase.firebaseConnect import FirebaseConnect
 
 # from src.TcpClient import tcpClient
 
-firebase = FirebaseConnect()
-firebase.listenData()
+# firebase = FirebaseConnect()
+# firebase.listenData()
 # firebase.addSensor(1)
 # firebase.readData()
 # firebase.deleteSensorData(0)
